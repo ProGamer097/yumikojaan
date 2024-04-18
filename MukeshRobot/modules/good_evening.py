d88b37f1fa777e4b074ef42f6a308c9f77616cae
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-18 20:51:29 +0530

    Update good_evening.py

--- MukeshRobot/modules/good_evening.py
+++ MukeshRobot/modules/good_evening.py
@@ -33,10 +33,10 @@ PHOTO = "https://graph.org/file/4126cda3b1ff1c43f70aa.mp4"
 
 @register(pattern=("Good evening"))
 async def awake(event):
-    NEKO = f"❀ ᴡɪsʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ ❀\n\n✦ ᴇᴠᴇɴɪɴɢs ᴀʀᴇ ᴛʜᴇ ʙᴇᴀᴜᴛɪғᴜʟʟʏ sᴡᴇᴇᴛ sᴘᴏᴛ ʙᴇᴛᴡᴇᴇɴ ᴛʜᴇ ʜᴀʀsʜ ʟɪɢʜᴛ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴀɴᴅ ᴛʜᴇ ᴅᴇᴀᴅ ᴅᴀʀᴋɴᴇss ᴏғ ɴɪɢʜᴛ.\n\n✦ ᴡɪsʜɪɴɢ ᴛᴏ ➛ {event.sender.first_name}\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [๛ᴀ ᴠ ɪ s ʜ ᴀ ༗](https://t.me/avishaxbot)"
+    NEKO = f"❀ ᴡɪsʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ ❀\n\n✦ ᴇᴠᴇɴɪɴɢs ᴀʀᴇ ᴛʜᴇ ʙᴇᴀᴜᴛɪғᴜʟʟʏ sᴡᴇᴇᴛ sᴘᴏᴛ ʙᴇᴛᴡᴇᴇɴ ᴛʜᴇ ʜᴀʀsʜ ʟɪɢʜᴛ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴀɴᴅ ᴛʜᴇ ᴅᴇᴀᴅ ᴅᴀʀᴋɴᴇss ᴏғ ɴɪɢʜᴛ.\n\n✦ ᴡɪsʜɪɴɢ ᴛᴏ ➛ {event.sender.first_name}\n\n✦ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ [sung gang](@sung_X_probot)"
     BUTTON = [
         [
-            Button.url("ᴍᴇᴇᴛ ᴍᴇ ʜᴇʀᴇ ʙᴀʙʏ", "https://telegram.dog/The_Friendz"),
+            Button.url("ᴍᴇᴇᴛ ᴍᴇ ʜᴇʀᴇ ʙᴀʙʏ", "https://t.me/naruto_support1"),
         ]
     ]
     await telethn.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
