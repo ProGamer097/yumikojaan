0e7cc7f3bedaed703a7c70f9f8afc362f27cb660
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-18 14:18:40 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -240,7 +240,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             
             x=update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAI33mLYLNLilbRI-sKAAob0P7koTEJNAAIOBAACl42QVKnra4sdzC_uKQQ")
+                "CAACAgIAAx0CfDXFXwACizpmIN2gvtprjupdFMn_M2jPJOBjXgACfBQAAl2TAUqcozeOx4snLR4E")
             x.delete()
             usr = update.effective_user
             lol = update.effective_message.reply_text(
